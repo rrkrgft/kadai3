36201335e1b42c78fdcc99cd4e035f04bb88653f
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-17 20:21:12 +0900

    User関係実装終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), notice: "登録しました"
+    else
+      render :new
+    end
   end
 
   def index
@@ -10,5 +21,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    binding.pry
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "画像を変更しました"
+    else
+      redirect_to user_path(@user.id)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
