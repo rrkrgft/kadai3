816fe55b7a48ec688cb7f59823931734a82fa6bd
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-17 10:58:52 +0900

    userのvaridation登録、ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'photos/new'
-  get 'photos/index'
-  get 'photos/show'
-  get 'photos/edit'
-  get 'users/new'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resouces :users
+  resouces :photos
+  resouces :sessions, only: [:new, :create, :destroy ]
+  resouces :favorites, only: [:create, :destroy ]
 end
