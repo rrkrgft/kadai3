509c958a41091755438248ecdd7f7384f6887634
kuniyuki isaji <rrkrgft@gmail.com>
2023-02-19 12:56:16 +0900

    photosのnewファイル作成

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,19 @@
 class PhotosController < ApplicationController
   def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to photos_path, notice: "登録しました"
+    else
+      render :new
+    end
   end
 
   def index
+    @photos = Photo.all
   end
 
   def show
@@ -10,4 +21,11 @@ class PhotosController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :text, :image_cache )
+  end
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
+  validates :image, presence: true
+  validates :text, presence: true, length: { maximum: 255 }
 end
